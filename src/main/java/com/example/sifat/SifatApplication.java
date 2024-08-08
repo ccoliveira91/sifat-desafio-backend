787278ec163626eb87834537a8557c545fa067a9
ccoliveira91 <cardoso.cesar91@outlook.com>
2024-08-08 19:11:19 -0300

    Initial commit for backend

---
+++ src/main/java/com/example/sifat/SifatApplication.java
@@ -0,0 +1,13 @@
+package com.example.sifat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SifatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SifatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sifat/SifatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sifat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SifatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
